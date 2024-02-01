4610ddd7a651e85eee7338f23536bf1f22fe7e94
reenact_l <77737814+jhlee343@users.noreply.github.com>
2024-02-01 16:46:41 +0900

    init

---
+++ src/test/java/shootingstar/socketmessage/SocketMessageApplicationTests.java
@@ -0,0 +1,13 @@
+package shootingstar.socketmessage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocketMessageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
