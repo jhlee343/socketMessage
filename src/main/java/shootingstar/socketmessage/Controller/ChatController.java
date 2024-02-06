0ecd4ac2fdca4934d38d952adb09f9005ab850e8
reenact_l <77737814+jhlee343@users.noreply.github.com>
2024-02-06 16:44:53 +0900

    quit message

--- src/main/java/shootingstar/socketmessage/Controller/ChatController.java
+++ src/main/java/shootingstar/socketmessage/Controller/ChatController.java
@@ -18,7 +18,7 @@ public class ChatController {
     private final ChatService chatService;
 
 
-    @RequestMapping("/chatList")
+    @RequestMapping("chat/chatList")
     public String chatList(Model model){
         List<ChatRoom> roomList = chatService.findAllRoom();
         model.addAttribute("roomList",roomList);
@@ -38,7 +38,7 @@ public class ChatController {
     public String chatRoom(Model model, @RequestParam Long roomId){
         ChatRoom room = chatService.findRoomById(roomId);
         model.addAttribute("room",room);
-        //현재 방에 들어오기위해서 필요한데...... 접속자 수 등등은 실시간으로 보여줘야 돼서 여기서는 못함
         return "chatRoom";
     }
+
 }
--- src/main/java/shootingstar/socketmessage/Service/ChatService.java
+++ src/main/java/shootingstar/socketmessage/Service/ChatService.java
@@ -8,7 +8,21 @@ import org.springframework.stereotype.Service;
 import shootingstar.socketmessage.DTO.ChatRoom;
 
 import java.util.*;
+/*
+내가한것
+    채팅방 구현 (1:n)
+    채팅방 목록 구현
+현재 구현문제
+    현재 채팅방 리스트가 제대로 안나오고 있음 해당 부분 파악해야함 - 해결
+    한번에 입장할지 container owner가 만드는 걸지는 생각해봐야함
+    채팅방 나가면 그대로 종료?
 
+추후 연결해야할 것
+    containerId 연결
+    roomId 연결( 현재 랜덤 값으로 넣어주고 있음)
+    containerId or roomId를 공유하는 사용자 쿼리를 써서 리스트로 받아와야하나
+
+*/
 @Slf4j
 @RequiredArgsConstructor
 @Service
